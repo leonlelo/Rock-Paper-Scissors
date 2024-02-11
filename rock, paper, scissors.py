d1c04682295fb8425582bac1712f6db1d793ab5b
leonlelo <158058447+leonlelo@users.noreply.github.com>
2024-02-11 22:03:58 +0100

    Update rock, paper, scissors.py
    Added so you can see what you were choosing, and what the robot were choosing:)

--- rock, paper, scissors.py
+++ rock, paper, scissors.py
@@ -7,43 +7,59 @@ playerinput = (input("Rock, Paper or Scissors? "))
 
 if playerinput == "Rock": #If player selected rock
     if choices == "Rock": #Draw
-        print("Draw")
+        print("Robot:",choices)
+        print("Player:",playerinput)
+        print("It's a draw")
 
     elif choices == "Paper": #Robot won
-        print("Robot Won")
+        print("Robot:", choices)
+        print("Player:", playerinput)
+        print("It's a victory for the player!")
 
     elif choices == "Scissors": #Player won
-        print("You Won")
+        print("Robot:",choices)
+        print("Player:",playerinput)
+        print("It's victory for the robot!")
 
-    else: #An extremaly rare error
+    else: #An extremely rare error
         print("Error")
         exit()
 
 
 elif playerinput == "Paper": #If player selected paper
     if choices == "Paper": #Draw
+        print("Robot:", choices)
+        print("Player:", playerinput)
         print("Draw")
 
     elif choices == "Rock": #Player won
-        print("You won")
+        print("Robot:", choices) 
+        print("Player:", playerinput) 
+        print("You won") 
 
     elif choices == "Scissors": #Robot won
+        print("Robot:", choices)
+        print("Player:", playerinput)
         print("Robot won")
 
     else: #Rare error
         print("Error")
         exit()
 
-
-
 elif playerinput == "Scissors": #If player selected scissors
     if choices == "Scissors": #Draw
+        print("Robot:", choices)
+        print("Player:", playerinput)
         print("Draw")
 
     elif choices == "Paper": #Player won
+        print("Robot:", choices)
+        print("Player:", playerinput)
         print("Player won")
 
-    elif choices == "Rock": #Robot won
+    elif choices == "Rock":
+        print("Robot:", choices)
+        print("Player:", playerinput)
         print("Robot won")
 
     else: #Rare error
@@ -51,12 +67,5 @@ elif playerinput == "Scissors": #If player selected scissors
         exit()
 
 else: #If player did not type rock paper or scissors
-    print("Error, try type Rock Paper or Scissors.")
+    print("Error, try type Rock Paper or Scissors, remember captital letters first.")
     exit()
-
-
-
-
-
-
-
